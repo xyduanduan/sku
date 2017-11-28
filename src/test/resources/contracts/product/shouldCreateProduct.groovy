f5395d6a0f70060dc9ae323e532f656c4caa7eaa
xyduan <xyduan@thoughtworks.com>
2017-11-28 16:38:15 +0800

    producer generate stubs

--- src/test/resources/contracts/product/shouldCreateProduct.groovy
+++ src/test/resources/contracts/product/shouldCreateProduct.groovy
@@ -1,4 +1,34 @@
 package product
 
-class shouldCreateProduct {
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method 'post'
+        url '/products'
+        body("""
+    {
+      "name":"1234567890",
+      "sku":99999,
+      "desc": "test"
+    }
+    """)
+        headers {
+            header('Content-Type', 'application/json')
+        }
+    }
+    response {
+        status 200
+        body("""
+  {
+    "fraudCheckStatus": "FRAUD",
+    "rejectionReason": "Amount too high"
+  }
+  """)
+        headers {
+            header('Content-Type': 'application/vnd.fraud.v1+json')
+        }
+    }
 }
+
